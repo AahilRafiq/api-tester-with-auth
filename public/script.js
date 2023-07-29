b65eebf29b39e5276272903824fa7705ec56f8f1
aahil <aahil.rafiq@gmail.com>
2023-07-29 09:14:59 +0530

    params and header section completed

--- public/script.js
+++ public/script.js
@@ -1,8 +1,14 @@
 
-
 // 1. Add new Parameter key pair value
 const addParamBtn = document.getElementById('addParamBtn')
 addParamBtn.addEventListener('click',()=>{
     const divCopy = document.getElementById('firstParam').cloneNode(true);
     document.getElementById('newParamsContainer').appendChild(divCopy);
+})
+
+// 2. Add new Header key pair value
+const addHeaderBtn = document.getElementById('addHeaderBtn')
+addHeaderBtn.addEventListener('click',()=>{
+    const divCopy = document.getElementById('firstHeader').cloneNode(true);
+    document.getElementById('newHeadersContainer').appendChild(divCopy);
 })
